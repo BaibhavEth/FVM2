c2340db634926a61ef061b32421c2a3e5103336f
baibhavthapa <baibhavt@buffalo.edu>
2023-12-24 14:18:01 -0500

    done

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+  },
+  paths: {
+    artifacts: "./artifacts",
+  },
+};
